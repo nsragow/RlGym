bb47be9048cd118bffba5b59282cb86c9fffd637
Noah Sragow <nsragow@gmail.com>
2019-12-05 18:17:27 -0500

    now working, changed some hyperparameters

--- random_search.py
+++ random_search.py
@@ -10,20 +10,22 @@ def get_random_policy(env):
 def random_policy_list(env,random_policy_count = 1000):
     random_policies = list()
     for _ in range(random_policy_count):
-        random_policies.append(get_random_policy)
+        random_policies.append(get_random_policy(env))
     return random_policies
 
 
 if __name__ == "__main__":
     env = gym.make("FrozenLake-v0")
-    policies = random_policy_list(env)
+    policies = random_policy_list(env,10000)
     best_performer = None
     best_performance = -1
+    count = 0
     for policy in policies:
-        score = evaluate_policy(env,policy)
+        score = evaluate_policy(env,policy,evaluations=10)
         if score > best_performance:
             best_performance = score
             best_performer = policy
+        count += 1
     print(score)
 
 
