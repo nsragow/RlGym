766d4e82b17900cf96bff12412d9fe32905d7d83
Noah Sragow <nsragow@gmail.com>
2019-12-05 18:34:19 -0500

    fixed bug where obsevations would not be updated

--- policy_evaluator.py
+++ policy_evaluator.py
@@ -5,12 +5,12 @@ def evaluate_policy(env, policy, max_steps=1000, evaluations = 100):
         step_count = 0
         reward = 0
         done = False
-        while (step_count > max_steps) and not done:
+        while (step_count < max_steps) and not done:
             action = policy[obs]
-            new_obs, additional_reward, is_done, info = env.step(action)
+            obs, additional_reward, is_done, info = env.step(action)
             done = is_done
             reward += additional_reward
             step_count += 1
-        weighted_reward = reward/step_count
-        score += weighted_reward
+
+        score += reward
     return score
