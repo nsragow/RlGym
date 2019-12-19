ecf8e22d8c80e8722f1060c6f9509f80b92f0931
Noah Sragow <nsragow@gmail.com>
2019-12-19 14:27:57 -0500

    evaluator for actors without policy extraction

---
+++ actor_evaluator.py
@@ -0,0 +1,25 @@
+def evaluate_actor(actor, max_steps=1000, evaluations=100, render=False):
+    score = 0
+    for _ in range(evaluations):
+        actor.reset()
+        step_count = 0
+        reward = 0
+        done = False
+        if render:
+            actor.env.render()
+        while (step_count < max_steps) and not done:
+            step_reward, step_done = actor.step(learning=False)
+            done = step_done
+            reward += step_reward
+            step_count += 1
+            if render:
+                actor.env.render()
+        score += reward
+    return score
+
+
+if __name__ == "__main__":
+    import pickle
+    file = open("./pickles/q_learner.pkl", 'rb')
+    actor = pickle.load(file)
+    print(f'score: {evaluate_actor(actor)}')
