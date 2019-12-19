073e398e5dabb0ddec41afe7eebf30c008f982c4
Noah Sragow <nsragow@gmail.com>
2019-12-19 14:37:04 -0500

    added learn script

---
+++ actor_learn.py
@@ -0,0 +1,11 @@
+if __name__ == '__main___':
+    import sys
+    import pickle
+    file = open(sys.argv[1], 'rb')
+    actor = pickle.load(file)
+    if len(sys.argv) > 2:
+        actor.learn(int(sys.argv[2]))
+    else:
+        actor.learn()
+    file = open(sys.argv[1], 'wb')
+    pickle.dump(actor, file)
