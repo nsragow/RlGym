9550953d2c76f0ea06915c53db52e1c87b1de69f
Noah Sragow <nsragow@gmail.com>
2019-12-29 13:47:41 -0500

    edited for dqn

--- actor_learn.py
+++ actor_learn.py
@@ -1,26 +1,35 @@
-lr = 0
+lr = .05
 
 if __name__ == '__main__':
     import sys
     import pickle
     import logging
-    from q_learning import QLearner
+
     import math
+
     from actor_evaluator import evaluate_actor
+    from dqn import DQNLearner
+
     file = open(sys.argv[1], 'rb')
     actor = pickle.load(file)
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     if len(sys.argv) > 2:
         try:
-            sprint_len = 1000
+            score = 0.0
+            sprint_len = 500
             steps = int(sys.argv[2])
             sprints = steps/sprint_len
             frac, whole_sprints = math.modf(sprints)
-            for _ in range(int(whole_sprints)):
-                actor.learn(sprint_len, lr=lr)
-                logging.info(f'Score: {evaluate_actor(actor)}')
-            actor.learn(int(frac*sprint_len), lr=lr)
-            logging.info(f'Final: {evaluate_actor(actor)}')
+
+            for i in range(int(whole_sprints)):
+
+                epsilon = ((whole_sprints - i) / whole_sprints) * .3
+                epsilon = .5  # could use either
+                lr = (1 - math.sqrt(i)/math.sqrt(whole_sprints)) * .2
+                actor.learn(sprint_len)
+                score = evaluate_actor(actor, max_steps=-1, evaluations=10)
+                logging.info(f'Score: {score}               lr: {lr} epsilon: {epsilon}  i: {i}/{whole_sprints}')
+
         except KeyboardInterrupt:
             logging.info('Saving...')
     else:
