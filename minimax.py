4c3409659232d917f6253c015db5f098b7d363b0
Noah Sragow <nsragow@gmail.com>
2020-01-05 18:55:13 -0500

    finished create game maker

--- minimax.py
+++ minimax.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, is_leaf=False):
+    def __init__(self, is_leaf=False, val=None):
         self.is_leaf = is_leaf
         self._value = None
         self.children = None
+        self.value = val
 
     def set_children(self, *children):
         self.children = []
@@ -17,10 +18,14 @@ class Node:
     def value(self):
         return self._value
 
+
 def create_minimax_game():
     root = Node()
     left = Node()
     right = Node()
 
+    root.set_children(left, right)
+    left.set_children(Node(val=3), Node(val=5))
+    right.set_children(Node(val=3), Node(val=5))
 
     return root
